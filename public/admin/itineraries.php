323be87ad8a1fa2f982609be700ec1d72e8dce5e
Ravi <patel.ravi04@gmail.com>
2019-09-13 15:09:46 -0500

    admin itinerary list, search and delete done

--- public/admin/itineraries.php
+++ public/admin/itineraries.php
@@ -1,11 +1,13 @@
 <?php
     /**
-     * Admin Tour Page 
-     * last_update: 2019-09-04
+     * Admin Itineraries Page 
+     * last_update: 2019-09-13
      * Author: Ravi Patel, patel-r89@webmail.uwinnipeg.ca
      */
     
     require __DIR__ . '/../../app/atg_config.php';
+
+    use App\ItinerariesModel;
 
     $title = 'ATG - Admin Itineraries';
     $heading = 'Itineraries';
@@ -18,15 +20,103 @@
         exit;
     }
 
+    $obj_itineraries = new ItinerariesModel;
+    $itineraries = $obj_itineraries->all('name', 'backend');
+
+    if('POST' == $_SERVER['REQUEST_METHOD']){
+        if(!empty($_POST['search'])){
+            $itineraries = $obj_itineraries->search($_POST['search']);
+            if(count($itineraries)>0){
+                $_SESSION['flash'] = count($itineraries) . " Itineraries(s) Found";
+                $_SESSION['flash_class'] = 'alert-success';
+            } else {
+                $_SESSION['flash'] = "No Itineraries(s) Found";
+                $_SESSION['flash_class'] = 'alert-info';
+            }
+        } else {
+            $_SESSION['flash'] = "Please type name to search a itineraries";
+            $_SESSION['flash_class'] = 'alert-info';
+            header('Location: itineraries.php');
+            die;
+        }
+    }
+
+    if(!empty($_GET['delete_itinerary'])){
+        $deleted = $obj_itineraries->delete($_GET['delete_itinerary']);
+        if($deleted>0){
+            $_SESSION['flash'] = "itinerary deleted successfully";
+            $_SESSION['flash_class'] = 'alert-success';
+            header('Location: itineraries.php');
+            die;
+        }
+    }
+
     // including head file
     require '../../inc/admin_head.inc.php';
 ?>
-
+        <?php require __DIR__ . '/../../inc/admin_flash.inc.php'; ?>
         <h1 class="dash-title"><?=esc($heading)?></h1>
+
+        <div class="row">
+            <div class="col">
+                <a href="manage_itinerary.php">
+                    <button type="button" class="btn btn-primary mb-1">
+                        Add New Itinerary
+                    </button>
+                </a>
+                <div class="card spur-card">
+                    <div class="card-header bg-secondary text-white">
+                        <div class="spur-card-icon">
+                            <i class="fas fa-table"></i>
+                        </div>
+                        <div class="spur-card-title">List of Itineraries</div>
+                    </div>
+                    <div class="card-body card-body-with-dark-table">
+                        <table class="table table-dark table-in-card" 
+                            id="table_itineraries">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $sr_no = 1;
+                                    foreach($itineraries as $itinerary):
+                                ?>
+                                    <tr>
+                                        <th scope="row"><?=esc($sr_no)?></th>
+                                        <td>
+                                            <?=esc($itinerary['name'])?>
+                                        </td>
+                                        <td class="description">
+                                            <?=esc($itinerary['description'])?>
+                                        </td>
+                                        <td>
+                                            <a href="manage_itinerary.php?itinerary_id=<?=esc_attr($itinerary['itinerary_id'])?>">
+                                                <button type="button" class="btn btn-primary btn-sm mb-1">Edit</button>
+                                            </a>
+                                            <a href="itineraries.php?delete_itinerary=<?=esc_attr($itinerary['itinerary_id'])?>"
+                                            onclick="return confirm('Are you sure to delete?')">
+                                                <button type="button" class="btn btn-danger btn-sm mb-1">Delete</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                <?php
+                                    $sr_no++;
+                                    endforeach;
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
                         
 <?php
-    require '../../inc/admin_under_construction.inc.php';
-    
     // including footer file
     require '../../inc/admin_footer.inc.php';
 ?>
